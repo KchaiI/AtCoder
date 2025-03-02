16e3409458c7093679e246140c01fcdd68fefa6e
kaichi0michael <ihciak.0127@gmail.com>
2025-03-02 17:15:11 +0900

    yet

--- B/MakeTarget.cpp
+++ B/MakeTarget.cpp
@@ -19,4 +19,11 @@ int main() {
         }  
         cout << "\n";
     }
+
+    for (int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++) {
+            cout << "#";
+        }
+        cout << "\n";
+    }
 }
