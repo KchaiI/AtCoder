446640b5d56a584797cf0fe5e798852506b18a01
kaichi0michael <ihciak.0127@gmail.com>
2025-03-03 00:36:44 +0900

    trying

---
+++ B/Theater.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int group;
+    cin >> group;
+
+    int num;
+    for (int i = 0; i < group; i++) {
+        int seat_from, seat_to;
+        cin >> seat_from >> seat_to;
+        num += seat_to - seat_from + 1;
+    }
+    cout << num;
+}
