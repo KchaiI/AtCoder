61ec34153489602096263b2636fb63827e132d24
kaichi0michael <ihciak.0127@gmail.com>
2025-03-08 07:08:58 +0900

    yet

--- B/PalindromicNumbers.cpp
+++ B/PalindromicNumbers.cpp
@@ -2,5 +2,13 @@
 using namespace std;
 
 int main() {
-    
+    int A, B, count;
+    cin >> A >> B;
+
+    for(int i = 0; i < B - A; i++) {
+
+        count ++;
+    }
+
+    cout << count << "\n";
 }
