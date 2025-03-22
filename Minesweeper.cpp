7a09113e0d91c8939209be24bb6c11078a4feb3f
kaichi0michael <ihciak.0127@gmail.com>
2025-03-22 23:51:44 +0900

    check bom

--- Minesweeper.cpp
+++ Minesweeper.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 
 int main() {
+    int h = 0, w = 0;
+    cin >> h >> w;
     
+    for(int i = 0; i < h; i++) {
+        for(int j = 0; j < w; j++) {
+            char n;
+            cin >> n;
+            if(n == '#') {
+
+            }
+        }
+    }
 }
