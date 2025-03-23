8b3e3cba19c4182ad556fa96651fccdc57174123
kaichi0michael <ihciak.0127@gmail.com>
2025-03-23 11:59:18 +0900

    stage make

--- Minesweeper.cpp
+++ Minesweeper.cpp
@@ -4,13 +4,21 @@ using namespace std;
 int main() {
     int h = 0, w = 0;
     cin >> h >> w;
+    vector<vector<int>> stage(h, vector<int>(w));
     
     for(int i = 0; i < h; i++) {
         for(int j = 0; j < w; j++) {
             char n;
             cin >> n;
             if(n == '#') {
-
+                stage.at(i).at(j + 1)++;
+                stage.at(i).at(j - 1)++;
+                stage.at(i - 1).at(j - 1);
+                stage.at(i - 1).at(j);
+                stage.at(i - 1).at(j + 1);
+                stage.at(i + 1).at(j - 1);
+                stage.at(i + 1).at(j);
+                stage.at(i + 1).at(j + 1);
             }
         }
     }
