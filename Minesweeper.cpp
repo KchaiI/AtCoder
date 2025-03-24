ffa24f6a92305d2e1de7483826720fb4b17b73ab
kaichi0michael <ihciak.0127@gmail.com>
2025-03-24 11:47:42 +0900

    error

--- Minesweeper.cpp
+++ Minesweeper.cpp
@@ -4,10 +4,11 @@ using namespace std;
 int main() {
     int h = 0, w = 0;
     cin >> h >> w;
-    vector<vector<int>> stage(h, vector<int>(w));
+    vector<vector<int>> data(3, vector<int>(4));
     
     for(int i = 0; i < h; i++) {
         for(int j = 0; j < w; j++) {
+            vector<vector<int>> stage(h, vector<int>(w));
             char n;
             cin >> n;
             if(n == '#') {
@@ -22,4 +23,6 @@ int main() {
             }
         }
     }
+
+    cout << stage << endl;
 }
