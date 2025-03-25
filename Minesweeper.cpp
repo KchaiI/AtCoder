8c04535458c01e5057b4b9cbfdd5565a4ffc0abf
kaichi0michael <ihciak.0127@gmail.com>
2025-03-25 18:30:45 +0900

    watch

--- Minesweeper.cpp
+++ Minesweeper.cpp
@@ -4,11 +4,10 @@ using namespace std;
 int main() {
     int h = 0, w = 0;
     cin >> h >> w;
-    vector<vector<int>> data(3, vector<int>(4));
+    vector<vector<int>> stage(h, vector<int>(w));
     
     for(int i = 0; i < h; i++) {
         for(int j = 0; j < w; j++) {
-            vector<vector<int>> stage(h, vector<int>(w));
             char n;
             cin >> n;
             if(n == '#') {
