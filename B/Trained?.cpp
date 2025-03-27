04af201613adf9eb2f33275f8f72409ab14b22b8
kaichi0michael <ihciak.0127@gmail.com>
2025-03-27 15:56:08 +0900

    yet

---
+++ B/Trained?.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n = 0, count = 0;
+    cin >> n;
+
+    //数値入力
+    for(int i = 0; i < n; i++) {
+        cin >> buttun[n];
+    }
+
+    //判定
+    cout << buttun[1];
+}
