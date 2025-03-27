44c211cc32e327cac9eeb936ff8528b31c8662a0
kaichi0michael <ihciak.0127@gmail.com>
2025-03-27 10:58:03 +0900

    half

--- B/CountingRoads.cpp
+++ B/CountingRoads.cpp
@@ -2,5 +2,14 @@
 using namespace std;
 
 int main() {
-    
+    int n = 0, m = 0;
+    cin >> n >> m;
+
+    vector<int> city(n);
+    for(int i = 0; i << m; i++) {
+        int a, b;
+        cin >> a >> b;
+        
+
+    }
 }
