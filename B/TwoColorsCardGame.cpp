4fea0e299e43310f25296955a3e96b1c54d2d2f9
kaichi0michael <ihciak.0127@gmail.com>
2025-03-29 22:46:40 +0900

    incert

--- B/TwoColorsCardGame.cpp
+++ B/TwoColorsCardGame.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 
 int main() {
-    
+    int n = 0, m = 0;
+    string si[n], ti[m];
+
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        cin >> si[i];
+        
+        cin >> m;
+        for(int j = 0; j < m; j++) {
+            cin >> ti[m];
+        }
+    }
 }
