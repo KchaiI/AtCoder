b41ac22b8d72542e049590a51fc2e0935a33d6de
kaichi0michael <ihciak.0127@gmail.com>
2025-03-29 12:57:10 +0900

    check about #

--- B/Minesweeper.cpp
+++ B/Minesweeper.cpp
@@ -4,15 +4,21 @@ using namespace std;
 int main() {
     int h = 0, w = 0;
     cin >> h >> w;
-    vector<vector<int>> stage(h, vector<int>(w));
+    vector<vector<int>> answer(h, vector<int>(w));
+    vector<vector<char>> stage(h, vector<char>(w));
+
+    int dx[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
+    int dy[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
     
     for(int i = 0; i < h; i++) {
         for(int j = 0; j < w; j++) {
-            char n;
-            cin >> n;
-            if(n == '#') {
-                
-            }
+            cin >> stage[i][j];
+        }
+    }
+
+    for(int i = 0; i < h; i++) {
+        for(int j = 0; j < w; j++) {
+            
         }
     }
 
