532a3f80990283f22c897650af6bbd8f62908066
kaichi0michael <ihciak.0127@gmail.com>
2025-03-31 18:10:37 +0900

    not yet

--- B/TwoColorsCardGame.cpp
+++ B/TwoColorsCardGame.cpp
@@ -2,16 +2,25 @@
 using namespace std;
 
 int main() {
-    int n = 0, m = 0;
-    string si[n], ti[m];
+    int n = 0, m = 0;       // カードの枚数
+    string si[n], ti[m];    // カードの文字入力
 
     cin >> n;
     for(int i = 0; i < n; i++) {
         cin >> si[i];
-        
-        cin >> m;
+    }
+
+    cin >> m;
+    for(int j = 0; j < m; j++) {
+        cin >> ti[m];
+    }
+
+    for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
-            cin >> ti[m];
+            
         }
     }
 }
+
+同じ文字列のカードの枚数を数えていく、
+比較してプラスになれば記憶
