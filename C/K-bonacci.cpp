5841038da617a223f73b3a1e125a6b1932afc49b
kaichi0michael <ihciak.0127@gmail.com>
2025-06-12 11:38:53 +0900

    正三角形の判定に誤り

---
+++ C/K-bonacci.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N, K;
+    int n = 0;
+    int a[n];
+
+    cin << N << K << endl;
+    for(int i = 0; i < N; i++) {
+        if(0 <= i && i < K) {
+
+        }
+    }
+}
