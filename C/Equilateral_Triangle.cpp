80e933e2ef0179d220c22cbfd39d91839564478a
kaichi0michael <ihciak.0127@gmail.com>
2025-06-12 18:39:30 +0900

    計算量多い

--- C/Equilateral_Triangle.cpp
+++ C/Equilateral_Triangle.cpp
@@ -16,16 +16,18 @@ int main() {
         position[i] = (position[i - 1] + d_i[i - 1]) % L;
     }
 
-    for(int i = 0; i < N;i++) {
-        cout << position[i] << endl;
-    }
+    // for(int i = 0; i < N;i++) {
+    //     cout << position[i] << endl;
+    // }
 
     int count = 0;
     int gap = L / 3;
     for(int a = 0; a < N - 2;a++) {
         for(int b = a + 1; b < N - 1;b++) {
             for(int c = b + 1; c < N; c++) {
-                if((position[b] - position[a] + L) == gap && (position[c] - position[b] + L) == gap && (position[a] - position[c] + L) == gap) {
+                vector<int> gapvec = {position[a], position[b], position[c]};
+                sort(gapvec.begin(), gapvec.end());
+                if((gapvec[1] - gapvec[0]) == gap && (gapvec[2] - gapvec[1]) == gap && (gapvec[0] - gapvec[2] + L) == gap) {
                     count++;
                 }
             }
