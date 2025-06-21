719fe96eaf5f7b2a3ca902bcd19658882903303c
kaichi0michael <ihciak.0127@gmail.com>
2025-06-21 21:23:30 +0900

    A done & B doing

---
+++ B/Distance_Table.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for(size_t i = 0; i < (n); ++i)
+using ll = long long;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> distance(n);
+    rep(i, n) cin >> distance[i];
+    
+    int size = 0;
+    rep(i, )
+}
